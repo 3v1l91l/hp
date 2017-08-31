f0ad4573d85f708db6e93838678425df42424706
Misha Zhukov <3v1l91l@gmail.com>
2017-08-31 23:19:42 +0300

    RR

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', function(req, res) {
-  // res.json({a:"b"});
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
-
-// console.log(`Server is listening on ${port}`);
