130889ed839b9f22848a813aee772f376b16dc3e
Misha Zhukov <3v1l91l@gmail.com>
2017-08-31 22:43:44 +0300

    RR

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ app.get('*', function(req, res) {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Server is listening on ${port}`);
+// console.log(`Server is listening on ${port}`);
